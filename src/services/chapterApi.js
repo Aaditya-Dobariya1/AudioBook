a01c76cc19c1b191a5c94a851df38c58f59b2340
Aaditya-Dobariya1 <aadityadobariya@gmail.com>
2024-09-11 14:54:42 +0530

    change chapterApi

--- src/services/chapterApi.js
+++ src/services/chapterApi.js
@@ -3,11 +3,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const chapterApi = createApi({
   reducerPath: "chapterApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://https://fb24-47-247-94-66.ngrok-free.app/",
+    baseUrl: "https://fb24-47-247-94-66.ngrok-free.app/",
   }),
   endpoints: (builder) => ({
     getChapters: builder.query({
-      query: () => "",
+      query: () => "book/8/chapters",
     }),
   }),
 });
